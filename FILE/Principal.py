d9e0da343eaea3279e7520be0475aef084e9abf8
Gabriel Costa Andrade <108695592+GaabrielCoosta@users.noreply.github.com>
2022-10-21 18:27:54 -0300

    Add License!

--- FILE/Principal.py
+++ FILE/Principal.py
@@ -1,7 +1,29 @@
+'''
+MIT License
+
+Copyright (c) 2022 Gabriel Costa Andrade
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
 from Uteis import menu
 from Uteis import cabecalho
-
-
 
 ciro = 0
 felipe = 0
@@ -17,7 +39,6 @@ soraya = 0
 vera = 0
 nulo = 0
 branco = 0
-
 
 while True:
 
@@ -50,9 +71,7 @@ while True:
             break
 
 
-
     if menuinicial == 1:
-
 
         while True:
             cabecalho("IR PARA VOTAÇÃO ?")
@@ -302,9 +321,5 @@ while True:
                         elif confirmar == "N":
                             cabecalho("Voto não confirmado!")
                             break
-
-
-
-
 
 cabecalho("Volte Sempre!")
